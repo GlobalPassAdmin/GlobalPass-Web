0ae5d901e2fd7d923e0083dc0050b4f2c43b6ed3
GlobalPassAdmin <omeraljbal@gmail.com>
2026-04-05 15:13:45 +0300

    Add initial Django settings configuration

---
+++ global_pass/settings.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-your-key-here' # غيرها لاحقاً
+DEBUG = False # اجعلها False عند النشر للعميل
+ALLOWED_HOSTS = ['*'] # للسماح بالوصول من أي رابط
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'services', # اسم تطبيق الخدمات
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # لإدارة ملفات التصميم
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'global_pass.urls'
+WSGI_APPLICATION = 'global_pass.wsgi.application'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
