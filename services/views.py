d6af02d221c1c41d67d249d65a689479ca8c0eda
GlobalPassAdmin <omeraljbal@gmail.com>
2026-04-05 19:52:18 +0300

    Update views.py

--- services/views.py
+++ services/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Service # استدعاء موديل الخدمات القديم
 
 def home(request):
+    # جلب جميع الخدمات المخزنة مسبقاً في قاعدة البيانات
     services = Service.objects.all()
+    
+    # إرسال البيانات إلى ملف التصميم index.html
     return render(request, 'index.html', {'services': services})
